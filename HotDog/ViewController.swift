782f073d9373f5a0bb98d590fa42d913d2024ac8
Jason0705 <6jasonli6@gmail.com>
2018-06-22 23:12:09 -0700

    Incorporate CoreMLModel

--- HotDog/ViewController.swift
+++ HotDog/ViewController.swift
@@ -28,12 +28,43 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = pickedImage
             
+            guard let ciImage = CIImage(image: pickedImage) else {
+                fatalError("Could not cover to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
     
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+    }
     
     
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
